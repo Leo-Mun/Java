27e95f0000f5cef24818f4e728e3b187446f3e19
Da Min Mun <mon04@naver.com>
2017-09-20 19:56:11 +0900

    Create CompressRunner.java

---
+++ Compress/CompressRunner.java
@@ -0,0 +1,12 @@
+package kr.cs.kookmin.opensouce;
+
+public class CompressRunner {
+	public static void main(String[] args) {
+		
+		Compress c = new Compress() ;
+		
+		c.compress("C:\\Users\\Moon\\workspace-\\OpenSourceProject\\src\\kr\\cs\\kookmin\\opensouce\\테스트.zip", "C:\\Users\\Moon\\workspace-\\OpenSourceProject\\src\\kr\\cs\\kookmin\\opensouce\\테스트");
+		
+		
+	}
+}
